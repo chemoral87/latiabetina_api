fbec1219292c1c1f9f65b815999c8e49beab1262
Sergio Morales <chemoral87@hotmail.com>
2024-10-08 13:33:49 -0600

    Initial commit

---
+++ resources/lang/es/messa.php
@@ -0,0 +1,83 @@
+<?php
+//NOTE español https://github.com/Laraveles/spanish
+return [
+  'role_create' => 'Rol creado',
+  'role_update' => 'Rol guardado',
+  'role_permission_update' => 'Permisos de Rol guardados',
+  'role_delete' => 'Rol eliminado',
+
+  'user_create' => 'Usuario creado',
+  'user_update' => 'Usuario guardado',
+  'user_roles_update' => 'Roles y permisos de Usuario guardados',
+  'user_delete' => 'Usuario eliminado',
+  'user_change' => 'Contraseña actualizada. Favor de volver a iniciar Sesión.',
+  'user_registration' => 'Registro Completado',
+
+  'profile_create' => 'Perfil creado',
+  'profile_update' => 'Perfil guardado',
+  'profile_delete' => 'Perfil eliminado',
+  'profile_favorite' => 'Perfil favorito',
+
+  'permission_create' => 'Permiso creado',
+  'permission_update' => 'Permiso guardado',
+  'permission_delete' => 'Permiso eliminado',
+
+  'investor_create' => 'Cuenta creada',
+  'investor_verification_code_send' => 'Código de verificación enviado',
+  'investor_verified' => 'Cuenta verificada',
+
+  'member_create' => 'Miembro creado',
+  'member_update' => 'Miembro guardado',
+  'member_delete' => 'Miembro eliminado',
+
+  'ministry_create' => 'Ministerio creado',
+  'ministry_update' => 'Ministerio guardado',
+  'ministry_delete' => 'Ministerio eliminado',
+
+  'member_address_create' => 'Dirección de Miembro(s) creada',
+  'member_call_create' => 'LLamada seguimiento creada',
+  'member_call_update' => 'LLamada seguimiento guardado',
+
+  'investor_profile_update' => 'Perfil guardado',
+
+  'credit_amortization_table' => 'Tabla de Amortización',
+
+  'faith_house_create' => 'Casa de Fe creada',
+  'faith_house_update' => 'Casa de Fe guardada',
+  'faith_house_delete' => 'Casa de Fe eliminada',
+
+  'faith_house_contact_create' => 'Contacto creado',
+  'faith_house_contact_delete' => 'Contacto eliminado',
+  'faith_house_contact_update' => 'Contacto guardado',
+
+  'faith_house_membership_create' => 'Solicitud enviada',
+  'faith_house_membership_delete' => 'Membresía eliminada',
+
+  'agro_event_create' => 'Agro Evento creado',
+  'agro_event_update' => 'Agro Evento guardado',
+  'agro_event_delete' => 'Agro Evento eliminado',
+
+  'organization_create' => 'Organización creada',
+  'organization_update' => 'Organización guardada',
+  'organization_delete' => 'Organización eliminada',
+
+  'organization_config_create' => 'Configuración guardada',
+
+  'attendant_create' => 'Servidor creado',
+  'attendant_update' => 'Servidor guardado',
+  'attendant_delete' => 'Servidor eliminado',
+
+  'church_service_create' => 'Servicio General creado',
+  'church_service_update' => 'Servicio General guardado',
+  'church_service_delete' => 'Servicio General eliminado',
+
+  'church_service_attendant_create' => 'Asignación completada',
+
+  'sky_registration_create' => 'Registro Creado',
+
+  'parking_car_create' => 'Auto creado',
+  'parking_car_update' => 'Auto guardado',
+  'parking_car_delete' => 'Auto eliminado',
+  'parking_car_not_found' => 'Auto no encontrado',
+
+];
