f61cbea50382f1c89d358109d335c1943e4a5a07
Sergio Morales <chemoral87@hotmail.com>
2026-06-18 16:58:29 -0600

    rename columns

--- database/migrations/2026_06_18_150906_rename_date_fields_in_church_events_table.php
+++ database/migrations/2026_06_18_150906_rename_date_fields_in_church_events_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
@@ -9,19 +10,15 @@ return new class extends Migration {
      * Run the migrations.
      */
     public function up(): void {
-        Schema::table('church_events', function (Blueprint $table) {
-            $table->renameColumn('start_date', 'publish_date');
-            $table->renameColumn('end_date', 'event_date');
-        });
+        DB::statement('ALTER TABLE church_events CHANGE COLUMN start_date publish_date DATE NOT NULL');
+        DB::statement('ALTER TABLE church_events CHANGE COLUMN end_date event_date DATE NULL');
     }
 
     /**
      * Reverse the migrations.
      */
     public function down(): void {
-        Schema::table('church_events', function (Blueprint $table) {
-            $table->renameColumn('publish_date', 'start_date');
-            $table->renameColumn('event_date', 'end_date');
-        });
+        DB::statement('ALTER TABLE church_events CHANGE COLUMN publish_date start_date DATE NOT NULL');
+        DB::statement('ALTER TABLE church_events CHANGE COLUMN event_date end_date DATE NULL');
     }
 };
